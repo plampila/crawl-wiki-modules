5dc091224541a0f13419ab927e411a7e7420e9e6
Pekka Lampila <pekka.lampila@iki.fi>
2016-11-02 12:29:30 +0100

    Spell: Add tables sorted by book or flag.
    Don't link to school if grouping by one.

--- spell.lua
+++ spell.lua
@@ -1,14 +1,32 @@
 local p = {}
 local data = mw.loadData('Module: Table of spells')
-local spell_names = {}
-for k in pairs(data) do
-  table.insert(spell_names, k)
-end
-table.sort(spell_names)
 
 local function table_has_value(t, v)
-  for _,x in pairs(t) do if x == v then return true end end
+  if t ~= nil then
+    for _,x in pairs(t) do if x == v then return true end end
+  end
   return false
+end
+
+local function table_keys_sorted(t)
+  local keys = {}
+  for k in pairs(t) do
+    table.insert(keys, k)
+  end
+  table.sort(keys)
+  return keys
+end
+
+local function names_by_level()
+  local names = table_keys_sorted(data)
+  table.sort(names, function(a, b)
+    if data[a].level == data[b].level then
+      return a < b
+    else
+      return data[a].level < data[b].level
+    end
+  end)
+  return names
 end
 
 local function spell_table_header()
@@ -52,10 +70,14 @@ local function spell_school_link(school)
   end
 end
 
-local function format_schools(schools)
+local function format_schools(schools, no_link_for)
   local ret = ''
-  for _,school in pairs(schools) do
-    ret = ret .. spell_school_link(school) .. '/'
+  for _,school in ipairs(schools) do
+    if school == no_link_for then
+      ret = ret .. school .. '/'
+    else
+      ret = ret .. spell_school_link(school) .. '/'
+    end
   end
   return ret:sub(1, -2)
 end
@@ -89,10 +111,10 @@ local function format_books(books)
   return ret:sub(1, -5)
 end
 
-local function spell_table_line(name, info)
+local function spell_table_line(name, info, no_link_for)
   return '|[[File:' .. name:lower() .. '.png]]\n' ..
          '|style="padding-left:1em"|[[' .. name .. ']]\n' ..
-         '|' .. format_schools(info['schools']) .. '\n' ..
+         '|' .. format_schools(info['schools'], no_link_for) .. '\n' ..
          '|' .. info['level'] .. '\n' ..
          '|' .. info['cap'] .. '\n' ..
          '|' .. format_range(info['range']) .. '\n' ..
@@ -107,7 +129,7 @@ function p.spell_table(frame)
 
   local ret = '==Spells==\n' .. spell_table_header()
   local c = nil
-  for _, name in ipairs(spell_names) do
+  for _, name in ipairs(table_keys_sorted(data)) do
       if name:sub(1, 1) ~= c then
         c = name:sub(1, 1)
         ret = ret .. spell_table_section(c)
@@ -119,42 +141,88 @@ end
 
 function p.spell_table_by_level(frame)
   local school = frame.args[1]
-  if school == "" then
+  if school == '' then
     school = nil
   end
   local ret = '==Spells==\n' .. spell_table_header()
   for level=1,9 do
     local found = false
-    for _, name in ipairs(spell_names) do if data[name]["level"] == level then
-      if school == nil or table_has_value(data[name]['schools'], school) then
-        if not found then
-          found = true
-          ret = ret .. spell_table_section('Level ' .. level)
+    for _, name in ipairs(table_keys_sorted(data)) do
+      if data[name]['level'] == level then
+        if school == nil or table_has_value(data[name]['schools'], school) then
+          if not found then
+            found = true
+            ret = ret .. spell_table_section('Level ' .. level)
+          end
+          ret = ret .. spell_table_line(name, data[name], school)
         end
-        ret = ret .. spell_table_line(name, data[name])
       end
-    end end
+    end
   end
   return ret .. '|}\n'
 end
 
 function p.spell_table_by_school(frame)
-  table.sort(spell_names, function(a, b)
-    if data[a].level == data[b].level then
-      return a < b
-    else
-      return data[a].level < data[b].level
-    end
-  end)
   local schools = { 'Air', 'Charms', 'Conjuration', 'Earth', 'Fire', 'Hexes',
     'Ice', 'Necromancy', 'Poison', 'Summoning', 'Translocation',
     'Transmutation' }
 
   local ret = '==Spells==\n' .. spell_table_header()
-  for _,school in pairs(schools) do
+  for _,school in ipairs(schools) do
     ret = ret .. spell_table_section(school)
-    for _, name in ipairs(spell_names) do
+    for _, name in ipairs(names_by_level(data)) do
       if table_has_value(data[name]['schools'], school) then
+        ret = ret .. spell_table_line(name, data[name], school)
+      end
+    end
+  end
+  return ret .. '|}\n'
+end
+
+function p.spell_table_by_book(frame)
+  local books = {}
+  for _,spell in pairs(data) do if spell['books'] ~= nil then
+    for _,book in pairs(spell['books']) do
+      books[book] = true
+    end
+  end end
+
+  local ret = '==Spells==\n' .. spell_table_header()
+  for _,book in ipairs(table_keys_sorted(books)) do
+    ret = ret .. spell_table_section('[[' .. book .. ']]')
+    for _, name in ipairs(names_by_level(data)) do
+      if table_has_value(data[name]['books'], book) then
+        ret = ret .. spell_table_line(name, data[name])
+      end
+    end
+  end
+  return ret .. '|}\n'
+end
+
+function p.spell_table_by_flag(frame)
+  local flags = {}
+  for _,spell in pairs(data) do if spell['flags'] ~= nil then
+    for _,flag in pairs(spell['flags']) do
+      flags[flag] = true
+    end
+  end end
+
+  local ret = '==Spells==\n' .. spell_table_header()
+
+  ret = ret .. spell_table_section('No flags')
+  for _, name in ipairs(names_by_level(data)) do
+    if data[name]['flags'] == nil then
+      ret = ret .. spell_table_line(name, data[name])
+    end
+  end
+
+  for _,flag in ipairs(table_keys_sorted(flags)) do
+    ret = ret .. spell_table_section(flag) ..
+      '| colspan=9 |' ..
+      frame:expandTemplate{title = 'SpellFlagDesc ' .. flag, args = {}} ..
+      '\n|----\n'
+    for _, name in ipairs(names_by_level(data)) do
+      if table_has_value(data[name]['flags'], flag) then
         ret = ret .. spell_table_line(name, data[name])
       end
     end
