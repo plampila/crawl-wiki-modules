326aa854b2293c90dbadd37876bfb2c03daa1a2c
Pekka Lampila <pekka.lampila@iki.fi>
2016-11-02 13:50:14 +0100

    Spellbook: New function to list spell sources for a school.

--- spellbook.lua
+++ spellbook.lua
@@ -7,6 +7,15 @@ local function table_has_value(t, v)
   return false
 end
 
+local function table_keys_sorted(t)
+  local keys = {}
+  for k in pairs(t) do
+    table.insert(keys, k)
+  end
+  table.sort(keys)
+  return keys
+end
+
 function p.spellbook_table(frame)
   local data = mw.loadData('Module:Table of spellbooks')
   local book = frame.args[1]
@@ -33,8 +42,9 @@ function p.short_spell_list(frame)
     return ""
   end
   local school = frame.args[2]
+  if school == "" then school = nil end
   local spell_data = nil
-  if school then
+  if school ~= nil then
     spell_data = mw.loadData('Module:Table of spells')
   end
   local result = "'''[[" .. book .. "]]''': "
@@ -46,6 +56,44 @@ function p.short_spell_list(frame)
   end
   result = result .. table.concat(spell_list, ", ")
   return result
+end
+
+function p.spell_sources(frame)
+  local spell_data = mw.loadData('Module:Table of spells')
+
+  local school = frame.args[1]
+  local primary_books = frame.args[2]
+
+  local done = {}
+  local ret = ""
+  if primary_books ~= nil and primary_books ~= "" then
+    ret = ret .. ";Main Texts\n"
+    for book in string.gmatch(primary_books, "%P+") do
+      ret = ret .. frame:expandTemplate{title = "spellbook2", args = {book, school}} .. "\n"
+      done[book] = true
+    end
+  end
+
+  local found = {}
+  for _,spell in pairs(spell_data) do
+    if table_has_value(spell['schools'], school) then
+      for _,book in pairs(spell['books']) do
+        if not done[book] and not found[book] then
+          found[book] = true
+        end
+      end
+    end
+  end
+
+  if next(found) ~= nil then
+    ret = ret .. ";Other Texts\n"
+    -- TODO: ignore "the" while sorting
+    for _,book in ipairs(table_keys_sorted(found)) do
+      ret = ret .. frame:expandTemplate{title = "spellbook2", args = {book, school}} .. "\n"
+    end
+  end
+
+  return ret:sub(1, -2)
 end
 
 return p
