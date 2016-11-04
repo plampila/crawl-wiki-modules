91a1b3dc31624b5996afbc5641268eb04a718e10
Pekka Lampila <pekka.lampila@iki.fi>
2016-11-04 11:40:40 +0100

    Spellbook: Add spellbook_info to get infobox and flavour.

--- spellbook.lua
+++ spellbook.lua
@@ -44,6 +44,37 @@ local function format_schools(schools, no_link_for)
   return ret:sub(1, -2)
 end
 
+local function main_school(book)
+  local schools = {}
+  for _,name in pairs(book.spells) do
+    for school in pairs(spell_data[name]['schools']) do
+      if schools[school] == nil then
+        schools[school] = 1
+      else
+        schools[school] = schools[school] + 1
+      end
+    end
+  end
+
+  local found = nil
+  for school,count in pairs(schools) do
+    if count == #book.spells then
+      if found ~= nil then return nil end
+      found = school
+    end
+  end
+  if found ~= nil then
+    return found
+  end
+  for school,count in pairs(schools) do
+    if count == #book.spells - 1 then
+      if found ~= nil then return nil end
+      found = school
+    end
+  end
+  return found
+end
+
 function p.spellbook_table(frame)
   local book = frame.args[1]
   if not book then
@@ -118,6 +149,36 @@ function p.spell_sources(frame)
   end
 
   return ret:sub(1, -2)
+end
+
+function p.spellbook_info(frame)
+  local name = frame.args[1]
+  if not name or name == "" then
+    name = mw.title.getCurrentTitle().text
+  end
+  name = name:gsub("^Book of", "book of")
+  local book = book_data[name]
+  if not book then
+    return name
+  end
+
+  local args = {}
+  args.name = book.name:gsub("^%l", string.upper)
+  args.rarity = book.rarity
+
+  local school = main_school(book)
+  if school ~= nil then
+    args.school = school
+  end
+
+  local infobox = frame:expandTemplate{title = 'book', args = args}
+
+  local flavour = book.description
+  if book.quote then
+    flavour = flavour .. '\n----\n' .. book.quote:gsub('\n', '<br>')
+  end
+  flavour = frame:expandTemplate{title = 'flavour', args = {flavour}}
+  return infobox .. '\n' .. flavour
 end
 
 return p
