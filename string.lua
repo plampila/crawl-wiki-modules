8adac8c41818f804e031150e6bc88ffa625bc8cf
Pekka Lampila <pekka.lampila@iki.fi>
2016-11-04 14:22:29 +0100

    String: Use single quote for strings.

--- string.lua
+++ string.lua
@@ -1,9 +1,9 @@
 local p = {}
- 
+
 -- Used in Template:Monster by Property:Hit_dice
 function p.first_word(frame)
   local text = frame.args[1]
-  for token in string.gmatch(text, "[^%s,]+") do
+  for token in string.gmatch(text, '[^%s,]+') do
     return token
   end
 end
@@ -11,8 +11,8 @@ end
 -- Used in Template:Monster by Property:Monster_magic_resistance
 function p.fix_magic_resistance(frame)
   local text = frame.args[1]
-  if text == "Immune" or text == "immune" then
-    return "1000"
+  if text == 'Immune' or text == 'immune' then
+    return '1000'
   end
   return text
 end
@@ -20,7 +20,7 @@ end
 -- Used in Template:Monster by Property:Monster_size
 function p.fix_monster_size(frame)
   local text = frame.args[1]
-  found, _, token = string.find(text, "|([^%]]+)%]")
+  found, _, token = string.find(text, '|([^%]]+)%]')
   if found then
     return token
   end
@@ -35,9 +35,8 @@ end
 -- Used in Template:Armour
 function p.fix_gdr(frame)
   local text = frame.args[1]
-  new_text, _ = string.gsub(text, "%%", "")
+  new_text, _ = string.gsub(text, '%%', '')
   return new_text
 end
-
 
 return p
