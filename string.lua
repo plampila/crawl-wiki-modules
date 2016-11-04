46b8f6675c73156a9b42be97d6ad665892218946
Pekka Lampila <pekka.lampila@iki.fi>
2016-11-04 14:19:18 +0100

    String: More functions. (CommanderC)

--- string.lua
+++ string.lua
@@ -1,10 +1,35 @@
 local p = {}
  
+-- Used in Template:Monster by Property:Hit_dice
 function p.first_word(frame)
   local text = frame.args[1]
   for token in string.gmatch(text, "[^%s]+") do
     return token
   end
+end
+
+-- Used in Template:Monster by Property:Monster_magic_resistance
+function p.fix_magic_resistance(frame)
+  local text = frame.args[1]
+  if text == "Immune" or text == "immune" then
+    return "1000"
+  end
+  return text
+end
+
+-- Used in Template:Monster by Property:Monster_size
+function p.fix_monster_size(frame)
+  local text = frame.args[1]
+  found, _, token = string.find(text, "|([^%]]+)%]")
+  if found then
+    return token
+  end
+  return text
+end
+
+-- Used in Template:Monster by Property:Monster_intelligence
+function p.fix_monster_intelligence(frame)
+  return p.fix_monster_size(frame)
 end
 
 return p
